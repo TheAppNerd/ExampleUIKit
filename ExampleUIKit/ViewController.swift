64f1ac71e896b5c971f9e1fb61c758f377c09393
Alex Thompson <alexthompsondevelopment@gmail.com>
2022-05-05 11:54:58 +1000

    implement UI

--- ExampleUIKit/ViewController.swift
+++ ExampleUIKit/ViewController.swift
@@ -9,19 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let label = UILabel()
+    let label     = UILabel()
+    let textField = UITextField()
+    let button    = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(label)
-        label.textAlignment  = .center
-        label.text           = "Hello, world!"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        setupLabel()
+        setupTextField()
+        setupButton()
+        layoutUI()
     }
 
+    func setupLabel() {
+        label.textAlignment  = .center
+        label.text           = ""
+    }
+    
+    func setupTextField() {
+        textField.delegate = self
+        textField.placeholder = "  Enter your name here"
+        textField.backgroundColor = .lightGray
+        textField.layer.cornerRadius = 10
+    }
+    
+    func setupButton() {
+        button.backgroundColor = .systemBlue
+        button.setTitle("Submit", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+    }
+    
+    @objc func buttonPressed(sender: UIButton) {
+        print("Button Pressed")
+    }
+    
+    func layoutUI() {
+        view.addSubview(label)
+        view.addSubview(textField)
+        view.addSubview(button)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 10
+        
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            textField.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -30),
+            textField.heightAnchor.constraint(equalToConstant: 50),
+            
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            button.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        label.text = textField.text
+    }
+
+    
 }
 
